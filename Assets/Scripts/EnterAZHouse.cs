138984b1c50191f369d7bc216c605bdcc93e73f7
RyanChroma <ttff2ryan@gmail.com>
2021-10-25 23:10:05 +0700

    E

---
+++ Assets/Scripts/EnterAZHouse.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterAZHouse : MonoBehaviour
+{
+    public GameObject E;
+    public bool colliderCheck;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        colliderCheck = true;
+        E.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        colliderCheck = false;
+        E.SetActive(false);
+    }
+
+    void Start()
+    {
+        colliderCheck = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && colliderCheck == true)
+        {
+            SceneManager.LoadScene("AZHouse");
+        }
+    }
+}
